def1dbe8b80f4eb0b8a712056d58dee083a9600b
FogSound1 <89969634+FogSound1@users.noreply.github.com>
2023-01-02 11:05:14 +0200

    Update Hive.cpp

--- Lab5/Hive.cpp
+++ Lab5/Hive.cpp
@@ -1,22 +1,87 @@
-#pragma once
-#include <random>
-#include <vector>
-#include "MatrixGraph.h"
-#include "Bee.h"
+#include "Hive.h"
 
-
-//Клас представлення вулику
-class Hive
+//Передається граф з містами задачі комівояжера
+Hive::Hive(MatrixGraph Graph) : Distance (Graph)
 {
-private:
-	int Population; //Кількість бджол
-	MatrixGraph Distance; //Граф з вершинами задачі комівояжера
-	random_device Device; //Прилад для генерації випадкових чисел
-	default_random_engine Engine; //Генератор випадкових чисел
-	vector <unique_ptr<Bee>> BeeHive; //Масив бджол
-public:
-	Hive(MatrixGraph Graph); //Конструктор класу
-	void CreateBees(); //Функція створення бджол
-	Route Waggle(int ForagerLimit, double ScoutCount, Route BestRoute); //Збір інформації
-	Route Recruit(Route BestRoute); //Перегляд найкращих результатів
-};
+	Population = 180;
+}
+
+//Функція створення бджол
+void Hive::CreateBees()
+{
+	int Onlooker_Count, Forager_Count;
+
+	Onlooker_Count = floor(Population * 0.5);
+	Forager_Count = Onlooker_Count;
+	BeeHive.resize(Population);
+	for (int i = 0; i < Onlooker_Count; i++)
+	{
+		BeeHive[i] = unique_ptr<Bee>(new Bee(1, Distance));
+	}
+
+	for (int i = Onlooker_Count; i < (Onlooker_Count + Forager_Count); i++)
+	{
+		BeeHive[i] = unique_ptr<Bee>(new Bee(2, Distance));
+		BeeHive[i]->ShuffleBeeRoute(Distance);
+	}
+}
+
+/*Збирається інформація на рахунок праці бджол - працівників
+Вибирається новий випадковий шлях для розвідників.*/
+Route Hive::Waggle(int ForagerLimit, double ScoutCount, Route BestRoute)
+{
+	int Iterator = 0;
+	vector<int> Indexes;
+	vector<double> ForagerDistances;
+	vector<double> ForagerDistancesSorted;
+	for (unique_ptr<Bee>& bee : BeeHive)
+	{
+		if (bee->GetRole() == 2)
+		{
+			bee->Forage(Distance, ForagerLimit);
+			if (bee->GetRoute().GetDistance() < BestRoute.GetDistance())
+			{
+				BestRoute.ChangePath(bee->GetRoute().GetPath(), bee->GetRoute().GetDistance());
+			}
+			Indexes.push_back(Iterator);
+			ForagerDistances.push_back(BestRoute.GetDistance());
+			ForagerDistancesSorted.push_back(BestRoute.GetDistance());
+		}
+		else if (bee->GetRole() == 3)
+		{
+			bee->Scout(Distance);
+		}
+		Iterator++;
+	}
+
+	vector<double>::iterator Ind;
+	sort(ForagerDistancesSorted.begin(), ForagerDistancesSorted.end(), greater<double>{});
+	for (int i = 0; i < ScoutCount && i < ForagerDistances.size(); i++)
+	{
+		Ind = find(ForagerDistances.begin(), ForagerDistances.end(), ForagerDistancesSorted[i]);
+		BeeHive[Indexes[Ind - ForagerDistances.begin()]]->SetRole(3);
+	}
+
+	return BestRoute;
+}
+
+/*Переглядається найкращі результати, які були до цього моменту отримані*/
+Route Hive::Recruit(Route BestRoute)
+{
+	for (unique_ptr<Bee>& bee : BeeHive)
+	{
+		if (bee->GetRole() == 1)
+		{
+			bee->SetRoute(BestRoute.GetPath(), BestRoute.GetDistance());
+			Route MutantRoute(0);
+			MutantRoute = bee->GetRoute().MutatePath(Distance);
+			bee->SetRoute(MutantRoute.GetPath(), MutantRoute.GetDistance());
+			if (bee->GetRoute().GetDistance() < BestRoute.GetDistance())
+			{
+				BestRoute.ChangePath(bee->GetRoute().GetPath(), bee->GetRoute().GetDistance());
+			}
+		}
+	}
+
+	return BestRoute;
+}
